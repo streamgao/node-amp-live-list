ed372677f618969afea48304ace163d0da615727
stream gao <streamg@yahoo-inc.com>
2017-06-23 13:04:33 -0700

    testing deletion and update content

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const bodyParser = require('body-parser');
 const moment = require('moment');
 
 // array of posts
-const posts = [];
+var posts = [];
 // number of post shown in a page
-const pageSize = 5;
+const pageSize = 15;
 
 const app = express();
 // set the view engine to ejs
@@ -19,39 +19,59 @@ app.get('/', (req, res) => {
   let currentPage = 1;
   const totalPosts = posts.length;
   const pageCount = Math.ceil(totalPosts / pageSize);
-  
+
   if(req.query.page) {
     currentPage = parseInt(req.query.page, 10);
   }
-  
+
   const start = (currentPage - 1) * pageSize;
   const end = currentPage * pageSize;
-	
+
   // render the `views/index.ejs` template with the list of posts (paginated)
-  res.render('index', 
-    {
+  res.render('index',{
 	  posts: posts.slice(start, end),
 	  pageSize: pageSize,
 	  pageCount: pageCount,
 	  currentPage: currentPage,
-    }
-  );
+  });
 });
 
-// POST endpoint to register a new post
-app.post('/new', (req, res) => {
+
+// endpoint to register a new post
+app.get('/new', (req, res) => {
   // Create new object
   const post = {
 	  id: posts.length + 1,
-	  videoID: req.body.videoID,
-	  timestamp: moment().unix(),
+	  contentID: posts.length + 1,
+	  timestamp: parseInt(moment().unix()) + 100,
 	  timestampStr: moment().format(),
   };
-  
+  console.log('\n........new........\n',posts.length,'\n...................\n');
+
   // Add the new element at the beginning of the array
   posts.unshift(post);
-  
-  res.status(200).json(post);
+  res.status(200).json(posts);
+});
+
+
+// endpoint to delete posts
+app.get('/delete', (req, res) => {
+
+  let i = Math.floor(Math.random() * posts.length);
+  let j = Math.floor(Math.random() * posts.length) - i -1;
+  posts.splice(i, j);
+  console.log(i, j, '\n........delete........\n',posts.length,'\n...................\n');
+
+  res.status(200).json(posts);
+});
+
+
+// endpoint to update a original post
+app.get('/update', (req, res) => {
+  let i = Math.floor(Math.random() * posts.length);
+  posts[i].contentID++ ;
+  console.log('\n........update........\n',posts[i],'\n...................\n');
+  res.status(200).json(posts);
 });
 
 app.listen(3000, () => {
